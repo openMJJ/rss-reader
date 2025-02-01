f5809b0342075d44d452cbd0208bfd4b59507857
Ray Wang <ray.wang@batechworks.com>
2025-02-01 19:59:09 +0800

    enhancement for MatchStr function

--- utils/match.go
+++ utils/match.go
@@ -6,11 +6,12 @@ import (
 )
 
 func MatchStr(str string, callback func(string)) {
+	strFinal := strings.ToLower(strings.TrimSpace(str))
 	for _, v := range globals.MatchList {
-		strFinal := strings.ToLower(strings.TrimSpace(str))
 		v = strings.ToLower(strings.TrimSpace(v))
 		if strings.Contains(strFinal, v) {
 			callback(str)
+			return
 		}
 	}
 }
